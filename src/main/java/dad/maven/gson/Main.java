61fe826c942e933bdc1ada92d993ba824584690c
Beatriz Garcia <beagh92@gmail.com>
2019-09-20 10:35:43 +0100

    Commit

--- src/main/java/dad/maven/gson/Main.java
+++ src/main/java/dad/maven/gson/Main.java
@@ -9,25 +9,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-	    Persona p = new Persona();
-	    System.out.println("Introduzca un nombre");
-	    p.setNombre(sc.nextLine());
-	    System.out.println("Introduzca unos apellidos");
-	    p.setApellidos(sc.nextLine());
-	    
-	    try {
-	    	System.out.println("Introduzca una edad");
-		    p.setEdad(sc.nextInt());
-        } catch (Exception e) {
-        	System.out.println("Edad incorrecta");
-        }
-	    
-	    sc.close();
-	    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-	    String json = gson.toJson(p);
-	    System.out.println(json);
-	    
-	    //System.out.println(p.toString());
+		Persona p = new Persona();
+		System.out.println("Introduzca un nombre");
+		p.setNombre(sc.nextLine());
+		System.out.println("Introduzca unos apellidos");
+		p.setApellidos(sc.nextLine());
+
+		try {
+			System.out.println("Introduzca una edad");
+			p.setEdad(sc.nextInt());
+		} catch (Exception e) {
+			System.out.println("Edad incorrecta");
+		}
+
+		sc.close();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(p);
+		System.out.println(json);
+
+		// System.out.println(p.toString());
 	}
 
 }
